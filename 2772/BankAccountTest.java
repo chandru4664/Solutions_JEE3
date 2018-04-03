f4d5f528c6c5112ad5d3a1389c544f33ad384ca4
Sindhureddyk29 <37079239+Sindhureddyk29@users.noreply.github.com>
2018-04-03 19:51:13 -0400

    Add files via upload

---
+++ 2772/BankAccountTest.java
@@ -0,0 +1,15 @@
+package bankacc.exercise;
+
+public class BankAccountTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		BankAccount	b1 = new BankAccount();
+		b1.setAccountNo("HDFC12345");
+		b1.setAccountName("Savings");
+		System.out.println(b1);
+		
+	}
+
+}
