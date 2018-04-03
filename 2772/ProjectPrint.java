2d523099fc2d4a6e91de42f0faf439aeb92a3cde
Sindhureddyk29 <37079239+Sindhureddyk29@users.noreply.github.com>
2018-04-03 19:21:18 -0400

    Add files via upload

---
+++ 2772/ProjectPrint.java
@@ -0,0 +1,13 @@
+package project.exercise;
+
+public class ProjectPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Project p1 = new Project("P1001","Exercise","Ram",50);
+		System.out.println(p1);
+		
+	}
+
+}
