943d83b8d6f13b8aae0d4c0ffee294cbf8b3ddb2
surajs3 <email2suraj@yahoo.com>
2018-04-02 21:30:56 -0400

    Add files via upload

---
+++ 1483/Batch.java
@@ -0,0 +1,43 @@
+/**
+ * HW1 b
+ */
+package htc.HomeWork1;
+import java.lang.System;
+import htc.HomeWork1.Trainee;
+
+/**
+ * @author 1483
+ *
+ */
+
+public class Batch {
+
+	/**
+	 * 
+	 */
+		int batchCode;
+		String startdate;
+		String enddate;
+		Trainee Trainee[];
+		
+		public Trainee getTrainee(int traineeId) throws TraineeNotFound {
+					
+			   return null;
+		}
+	
+	       
+		public Trainee[] getTrainee(String gender) {
+			System.out.println(Trainee);
+			return Trainee;
+			
+		}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
