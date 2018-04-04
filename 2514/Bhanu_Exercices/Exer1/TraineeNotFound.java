03eed60472a1ea618bd8e5292c5f6e1fee635c2c
bbhanu1209 <36871443+bbhanu1209@users.noreply.github.com>
2018-04-03 21:17:25 -0400

    Add files via upload

---
+++ 2514/Bhanu_Exercices/Exer1/TraineeNotFound.java
@@ -0,0 +1,19 @@
+package com.htc.pack;
+
+public class TraineeNotFound extends Exception {
+	
+	String error_message="Trainee Not Found";
+	
+	
+	public TraineeNotFound() {
+		
+		super();
+		
+	}
+
+	/*public TraineeNotFound(String message) {
+		super(message);
+		this.error_message=message;
+		
+	}*/
+}
